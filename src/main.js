4024d8a5dcbe4df706eb748acd35064a729d338c
Kirbymania <kirbymania2000@gmail.com>
2022-04-19 01:02:17 -0700

    added up points

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 // Em Coo
 // Rocket Patrol Mods
 // 4/18/2022
-// ~11 hours
+// ~12 hours
 
 // Point Breakdown
+// Track a high score that persists across scenes and display it in the UI (5)
 // Implement the 'FIRE' UI text from the original game (5)
 // Add your own (copyright-free) background music to the Play scene (5)
 // Randomize each spaceship's movement direction at the start of each play (5)
@@ -13,7 +14,7 @@
 // Implement parallax scrolling (10)
 // Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
 // Create new artwork for all of the in-game assets (rocket, spaceships, explosion) (20) 
-// 
+// Total: 95 Points
 
 console.log("Hello from main.js");
 
